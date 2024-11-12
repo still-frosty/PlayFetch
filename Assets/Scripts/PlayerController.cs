f07a9d68b9c0d94863f4d9b09242b719c34ac7cb
Opeyemi <64136215+still-frosty@users.noreply.github.com>
2024-11-12 03:42:08 +0100

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,12 +9,7 @@ public class PlayerController : MonoBehaviour
     public float xRange = 10.0f;
     public GameObject[] projectilePrefab;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
